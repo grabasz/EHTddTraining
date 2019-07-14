4b049b48ba6aea9b931eacd5ee7ad90ca640d24f
Bartlomiej Grabowski <bartlomiej.grabowski@globallogic.com>
2019-07-14 17:20:00 +0200

    Refactored unit tests

--- VideoStore/VideoStore/test.cpp
+++ VideoStore/VideoStore/test.cpp
@@ -4,107 +4,103 @@
 
 class CustomerTest : public testing::Test
 {
+public:
+	std::shared_ptr<Customer> _customer;
+	std::shared_ptr<Movie> _regularMovie ;
+	std::shared_ptr<Movie> _newReleaseMovie ;
+	std::shared_ptr<Movie> _childrenMovie ;
 	void SetUp() override
 	{
+		_customer = std::make_shared<Customer>("Client");
+		_regularMovie = std::make_shared<Movie>("Gone with the Wind", Movie::REGULAR);
+		_newReleaseMovie = std::make_shared<Movie>("Star Wars", Movie::NEW_RELEASE);
+		_childrenMovie = std::make_shared<Movie>("Madagascar", Movie::CHILDRENS);
 	};
 
 	void TearDown() override
 	{
+		_customer.reset();
+		_regularMovie.reset();
+		_newReleaseMovie.reset();
+		_childrenMovie.reset();
 	};
 };
 
 TEST_F(CustomerTest, TestCustomer)
 {
-	Customer* c = new Customer;
-	ASSERT_NE(nullptr, c);
-	delete c;
+	ASSERT_NE(nullptr, _customer.get());
 }
 
 TEST_F(CustomerTest, TestAddRental)
 {
-	Customer customer2 = Customer("Julia");
-	Movie movie1 = Movie("Gone with the Wind", Movie::REGULAR);
-	Rental rental1 = Rental(movie1, 3); // 3 day rental
-	customer2.addRental(rental1);
+	Rental rental1 = Rental(*_regularMovie, 3); // 3 day rental
+	_customer->addRental(rental1);
 }
 
 TEST_F(CustomerTest, TestGetName)
 {
-	Customer c = Customer("David");
-	ASSERT_EQ("David", c.getName());
+	ASSERT_EQ("Client", _customer->getName());
 }
-
 
 TEST_F(CustomerTest, StatementForRegularMovie)
 {
-	Movie movie1 = Movie("Gone with the Wind", Movie::REGULAR);
-	Rental rental1 = Rental(movie1, 3); // 3 day rental
-	Customer customer2 = Customer("Sallie");
-	customer2.addRental(rental1);
-
-	std::string expected = "Rental Record for Sallie\n"
+	Rental rental1 = Rental(*_regularMovie, 3); // 3 day rental
+	_customer->addRental(rental1);
+	std::string expected = "Rental Record for Client\n"
 		"\tGone with the Wind\t3.5\n"
 		"Amount owed is 3.5\n"
 		"You earned 1 frequent renter points";
-	const std::string statement = customer2.statement();
+	
+	const std::string statement = _customer->statement();
+	
 	ASSERT_EQ(expected, statement);
 }
 
 TEST_F(CustomerTest, StatementForNewReleaseMovie)
 {
-		Movie movie1 = Movie("Star Wars", Movie::NEW_RELEASE);
-	Rental rental1 = Rental(movie1, 3); // 3 day rental
-	Customer customer2 = Customer("Sallie");
-	customer2.addRental(rental1);
-		
-	std::string expected = "Rental Record for Sallie\n"
+	Rental rental1 = Rental(*_newReleaseMovie, 3); // 3 day rental
+	_customer->addRental(rental1);
+	std::string expected = "Rental Record for Client\n"
 		"\tStar Wars\t9\n"
 		"Amount owed is 9\n"
 		"You earned 2 frequent renter points";
-	std::string statement = customer2.statement();
+	
+	std::string statement = _customer->statement();
 
 	ASSERT_EQ(expected, statement);
 }
 
-
 TEST_F(CustomerTest, StatementForChildrensMovie)
 {
-		Movie movie1 = Movie("Madagascar", Movie::CHILDRENS);
-		Rental rental1 = Rental(movie1, 3); // 3 day rental
-		Customer customer2 = Customer("Sallie");
-
-		customer2.addRental(rental1);
-			
-		std::string expected = "Rental Record for Sallie\n"
+		Rental rental1 = Rental(*_childrenMovie, 3); // 3 day rental
+		_customer->addRental(rental1);
+		std::string expected = "Rental Record for Client\n"
 			"\tMadagascar\t1.5\n"
 			"Amount owed is 1.5\n"
 			"You earned 1 frequent renter points";
-		std::string statement = customer2.statement();
+
+		std::string statement = _customer->statement();
 
 		ASSERT_EQ(expected, statement);
 }
 
 TEST_F(CustomerTest, StatementForManyMovies)
 {
-		Movie movie1 = Movie("Madagascar", Movie::CHILDRENS);
-	Rental rental1 = Rental(movie1, 6); // 6 day rental
-	Movie movie2 = Movie("Star Wars", Movie::NEW_RELEASE);
-	Rental rental2 = Rental(movie2, 2); // 2 day rental
-	Movie movie3 = Movie("Gone with the Wind", Movie::REGULAR);
-	Rental rental3 = Rental(movie3, 8); // 8 day rental
-	Customer customer1
-		= Customer("David");
-	customer1.addRental(rental1).
+	Rental rental1 = Rental(*_childrenMovie, 6); // 6 day 
+	Rental rental2 = Rental(*_newReleaseMovie, 2); // 2 day rental
+	Rental rental3 = Rental(*_regularMovie, 8); // 8 day rental
+	
+	_customer->addRental(rental1).
 		addRental(rental2).
 		addRental(rental3);
 
-	std::string expected = "Rental Record for David\n"
+	std::string expected = "Rental Record for Client\n"
 		"\tMadagascar\t6\n"
 		"\tStar Wars\t6\n"
 		"\tGone with the Wind\t11\n"
 		"Amount owed is 23\n"
 		"You earned 4 frequent renter points";
-	std::string statement = customer1.statement();
-	ASSERT_EQ(expected, statement);
+	std::string statement = _customer->statement();
 
+	ASSERT_EQ(expected, statement);
 }
